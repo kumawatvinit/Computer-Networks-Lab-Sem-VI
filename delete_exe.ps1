3232c8f4b18d109259b8d0c734671c9f3de6cdad
kumawatvinit <vinitkumawat112@gmail.com>
2023-04-12 10:51:37 +0530

    initial commit

--- delete_exe.ps1
+++ delete_exe.ps1
@@ -1,1 +1,9 @@
-Get-ChildItem -Path . -Filter *.exe -Recurse | Remove-Item
+# Find all files with the extensions ".exe" and with no extension in the current directory and all its subdirectories
+$files = Get-ChildItem -Path . -File -Recurse | Where-Object { $_.Extension -eq ".exe" -or $_.Extension -eq "" }
+
+# Loop through each file and delete it
+foreach ($file in $files) {
+    if ($file.Extension -eq ".exe" -or $file.Extension -eq "") {
+        Remove-Item $file.FullName -Force
+    }
+}
